a0e17792b1d4aafe2f4b8d3e88c5c116eff64579
Anoop Vishvakarma <kumarvishwakarmaanoop68@gmail.com>
2026-09-12 23:25:36 +0530

    Add Python data types example

---
+++ 01-basics/data_types.py
@@ -0,0 +1,11 @@
+# Python Data Types
+
+name = "Anoop"          # str
+age = 18                 # int
+height = 5.6             # float
+is_student = True        # bool
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
