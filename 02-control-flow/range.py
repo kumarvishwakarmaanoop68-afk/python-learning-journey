99ddadca9e63a09abb475dceb6a929422747256f
Anoop Vishvakarma <kumarvishwakarmaanoop68@gmail.com>
2026-09-13 10:27:54 +0530

    Add Day 2 range practice

---
+++ 02-control-flow/range.py
@@ -0,0 +1,13 @@
+# range() examples
+
+print("0 to 4:")
+for number in range(5):
+    print(number)
+
+print("2 to 6:")
+for number in range(2, 7):
+    print(number)
+
+print("Even numbers from 2 to 10:")
+for number in range(2, 11, 2):
+    print(number)
