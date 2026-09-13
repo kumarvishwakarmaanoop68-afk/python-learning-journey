37d64eeac91c41d38ed816fadf68d3b0924ff78d
Anoop Vishvakarma <kumarvishwakarmaanoop68@gmail.com>
2026-09-13 10:27:38 +0530

    Add Day 2 if else practice

---
+++ 02-control-flow/if_else.py
@@ -0,0 +1,12 @@
+# Conditional statements
+
+marks = 72
+
+if marks >= 80:
+    print("Grade: A")
+elif marks >= 60:
+    print("Grade: B")
+elif marks >= 40:
+    print("Grade: C")
+else:
+    print("Grade: Fail")
