b9255d330772be6722bb11488a6247d2dfa40331
Anoop Vishvakarma <kumarvishwakarmaanoop68@gmail.com>
2026-09-14 22:16:55 +0530

    Add Day 3 dictionaries practice

---
+++ 03-data-structures/dictionaries.py
@@ -0,0 +1,17 @@
+# Dictionaries practice
+
+student = {
+    "name": "Anoop",
+    "course": "Diploma IT",
+    "skill": "Python"
+}
+
+print("Student:", student)
+print("Name:", student["name"])
+
+student["level"] = "Beginner"
+student["skill"] = "Python + C++"
+
+print("Updated:", student)
+print("Keys:", student.keys())
+print("Values:", student.values())
