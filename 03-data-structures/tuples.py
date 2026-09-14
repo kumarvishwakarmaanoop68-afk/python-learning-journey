e340d5ab540229dd7e50fabdd05014da193da7ec
Anoop Vishvakarma <kumarvishwakarmaanoop68@gmail.com>
2026-09-14 22:16:45 +0530

    Add Day 3 tuples practice

---
+++ 03-data-structures/tuples.py
@@ -0,0 +1,9 @@
+# Tuples practice
+
+numbers = (10, 20, 30, 40)
+
+print("Tuple:", numbers)
+print("First item:", numbers[0])
+print("Last item:", numbers[-1])
+print("Slice:", numbers[1:3])
+print("Length:", len(numbers))
