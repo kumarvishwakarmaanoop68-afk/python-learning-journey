7cea8d972658ea6777498f0865e87ec8777f345c
Anoop Vishvakarma <kumarvishwakarmaanoop68@gmail.com>
2026-09-14 22:16:35 +0530

    Add Day 3 strings practice

---
+++ 03-data-structures/strings.py
@@ -0,0 +1,11 @@
+# Strings practice
+
+text = "Python Learning"
+
+print("Original:", text)
+print("First character:", text[0])
+print("Last character:", text[-1])
+print("Slice:", text[0:6])
+print("Upper:", text.upper())
+print("Lower:", text.lower())
+print("Length:", len(text))
