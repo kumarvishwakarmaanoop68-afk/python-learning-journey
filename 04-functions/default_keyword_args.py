f54534c93a2691351646d0f4c48eb34918604a05
Anoop Vishvakarma <kumarvishwakarmaanoop68@gmail.com>
2026-09-15 08:15:55 +0530

    Add Day 4 default and keyword arguments

---
+++ 04-functions/default_keyword_args.py
@@ -0,0 +1,9 @@
+# Default and keyword arguments
+
+def student_info(name, course="Diploma IT"):
+    print("Name:", name)
+    print("Course:", course)
+
+
+student_info("Anoop")
+student_info(name="Anoop", course="Python")
