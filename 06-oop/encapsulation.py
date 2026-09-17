bd90017cd17e01fc5d5fc484f7dad69f48b1f15e
Anoop Vishvakarma <kumarvishwakarmaanoop68@gmail.com>
2026-09-17 21:49:51 +0530

    Add Day 6 encapsulation example

---
+++ 06-oop/encapsulation.py
@@ -0,0 +1,20 @@
+# Encapsulation
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+
+account = BankAccount("Student", 1000)
+account.deposit(500)
+
+print("Owner:", account.owner)
+print("Balance:", account.get_balance())
